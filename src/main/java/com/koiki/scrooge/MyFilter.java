613ef4cb26979db746483c106dfc71a294b4dd2f
takaaki.iida <takiida0605@gmail.com>
2017-11-01 19:59:49 +0900

    complted json-schema

--- src/main/java/com/koiki/scrooge/MyFilter.java
+++ src/main/java/com/koiki/scrooge/MyFilter.java
@@ -1,12 +1,11 @@
 package com.koiki.scrooge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 import java.io.IOException;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -14,76 +13,78 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class MyFilter implements Filter {
-	/**
-	 * https://stackoverflow.com/questions/10898056/how-to-find-all-controllers-in-spring-mvc
-	 */
-	@Autowired
-	private RequestMappingHandlerMapping handlerMapping;
-	@Autowired
-	private ObjectMapper mapper;
+	private final RequestMappingHandlerMapping handlerMapping;
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		System.out.println("init!!");
-	}
+	public void init(FilterConfig filterConfig) throws ServletException { }
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("Before!!");
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		String jsonSchema = httpRequest.getHeader("json-schema");
-		Map<RequestMappingInfo, HandlerMethod> aaa = handlerMapping.getHandlerMethods();
-		log.info(aaa.toString());
-		RequestMappingInfo requestInfo = new RequestMappingInfo(
-				new PatternsRequestCondition("/events"),
-				new RequestMethodsRequestCondition(RequestMethod.GET),
-				null,
-				null,
-				null,
-				null,
-				null);
-		Map.Entry<RequestMappingInfo, HandlerMethod> bbb = aaa.entrySet()
-				.stream()
-				.filter(e -> e.getKey().getPatternsCondition().getPatterns().contains("/events")
-							 && e.getKey().getMethodsCondition().getMethods().contains(RequestMethod.POST))
-				.findFirst()
-				.orElse(null);
-		log.info("test:" + bbb.toString());
 
-		HandlerMethod ccc = bbb.getValue();
-Class<?> requestClazz = ccc.getMethodParameters()[0].getParameterType();
+		try {
 
-		mapper.registerModule(new Hibernate5Module());
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-		JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
-		JsonSchema schema = schemaGen.generateSchema(requestClazz);
-String schemaString = mapper.writeValueAsString(schema);
-		log.info(schemaString);
+			if (!"true".equalsIgnoreCase(httpRequest.getHeader("json-schema"))) {
+				// proceed the normal process
+				chain.doFilter(request, response);
 
-		if (! "true".equals(jsonSchema)) {
+			} else {
+				// proceed to return JSON schema
+				Optional<Map.Entry<RequestMappingInfo, HandlerMethod>> matchedRequestMappingEntry = handlerMapping.getHandlerMethods()
+						.entrySet()
+						.stream()
+						.filter(e -> e.getKey().getMatchingCondition(httpRequest) != null)
+						.findFirst();
+
+				if (!matchedRequestMappingEntry.isPresent()) {
+					// proceed the normal process
+					log.debug("could not find matched request mapping, request path: {}, request method: {}",
+							httpRequest.getPathInfo(), httpRequest.getMethod());
+					chain.doFilter(request, response);
+
+				} else {
+					HandlerMethod handlerMethod = matchedRequestMappingEntry.get().getValue();
+					Class<?> requestBodyClass = handlerMethod.getMethodParameters()[0].getParameterType();
+
+					JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
+					JsonSchema schema = schemaGen.generateSchema(requestBodyClass);
+					String schemaString = mapper.writeValueAsString(schema);
+					log.info(schemaString);
+
+					// return JSON Schema
+					response.getWriter().print(schemaString);
+				}
+			}
+
+		} catch (Exception e) {
+			log.error("Exception happened when proceeding json-schema", e);
 			chain.doFilter(request, response);
-		} else {
-			response.getWriter().print(schemaString);
 		}
-		System.out.println("After!!");
 	}
 
 	@Override
-	public void destroy() {
-		System.out.println("destroy!!");
+	public void destroy() { }
+
+	/**
+	 * You can set your own object mapper from super class
+	 *
+	 * @param mapper
+	 */
+	protected void setObjectMapper(ObjectMapper mapper) {
+		this.mapper = mapper;
 	}
 }
--- src/main/java/com/koiki/scrooge/event/EventReq.java
+++ src/main/java/com/koiki/scrooge/event/EventReq.java
@@ -3,10 +3,13 @@ package com.koiki.scrooge.event;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
 public class EventReq {
 	@NotEmpty(message = "ERROR0001")
+	//@Min(1)
+	//@Length(min = 1, max = 5)
 	private String name;
 }
