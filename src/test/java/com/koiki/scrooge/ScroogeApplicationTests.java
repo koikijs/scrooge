1c28f2d234e695f5e881def979ce27f51b35dbb3
takaaki.iida <takiida0605@gmail.com>
2017-11-29 22:13:57 +0900

    add sample integration test with embedded mongo

--- src/test/java/com/koiki/scrooge/ScroogeApplicationTests.java
+++ src/test/java/com/koiki/scrooge/ScroogeApplicationTests.java
@@ -1,16 +1,34 @@
 package com.koiki.scrooge;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ScroogeApplicationTests {
+	@LocalServerPort
+	private int port;
 
 	@Test
-	public void contextLoads() {
+	public void sample() {
+		RestTemplate template = new RestTemplate();
+
+		Health result = template.getForObject("http://localhost:" + port + "/application/health", Health.class);
+
+		log.info("result: {}", result);
 	}
 
 }
+
+@Data
+class Health {
+	String status;
+}
+
