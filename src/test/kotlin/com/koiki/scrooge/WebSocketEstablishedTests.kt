5fc4f2ce41881be2a45701c52d99626d69f29fb2
takaaki.iida <takiida0605@gmail.com>
2017-12-02 18:32:34 +0900

    polish

--- src/test/kotlin/com/koiki/scrooge/WebSocketEstablishedTests.kt
+++ src/test/kotlin/com/koiki/scrooge/WebSocketEstablishedTests.kt
@@ -2,31 +2,24 @@ package com.koiki.scrooge
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.readValues
 import com.koiki.scrooge.event.EventReq
 import com.koiki.scrooge.event.EventRes
-import com.koiki.scrooge.scrooge.Scrooge
 import com.koiki.scrooge.scrooge.ScroogeReq
 import lombok.extern.slf4j.Slf4j
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.web.client.getForObject
-import org.assertj.core.api.Assertions.assertThat;
-import org.json.JSONObject
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.web.reactive.socket.WebSocketHandler
 import org.springframework.web.reactive.socket.WebSocketMessage
-import org.springframework.web.reactive.socket.WebSocketSession
 import org.springframework.web.reactive.socket.client.StandardWebSocketClient
-import reactor.core.publisher.Mono
 import reactor.core.publisher.ReplayProcessor
+import reactor.core.publisher.toMono
 import java.net.URI
-import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.regex.Pattern
@@ -49,16 +42,14 @@ class WebSocketEstablishedTests(
             .rootUri("http://localhost:$port")
             .build()
 
-    val webSocketUri = "ws://localhost:$port/?"
+    private val webSocketUri = "ws://localhost:$port/?"
 
-    var id: String = ""
+    private val objectMapper = ObjectMapper().registerModule(JavaTimeModule())
 
-    val objectMapper = ObjectMapper().registerModule(JavaTimeModule())
-
-    val expectedWsMessage = objectMapper.readValue("""
+    private val expectedWsMessage = objectMapper.readValue("""
         {
             "name": "Koiki Camp",
-            "id": "",
+            "id": "5a226c2d7c245e14f33fc5a8",
             "createdAt": "2017-12-02T16:52:45.52",
             "updatedAt": "2017-12-02T16:52:45.52",
             "scrooges": [
@@ -66,8 +57,8 @@ class WebSocketEstablishedTests(
                     "memberName": "Nabnab",
                     "paidAmount": 200,
                     "forWhat": "rent-a-car",
-                    "id": "",
-                    "eventId": "",
+                    "id": "5a226c2d7c245e14f33fc5a8",
+                    "eventId": "5a226c2d7c245e14f33fc5a8",
                     "createdAt": "2017-12-02T16:52:45.52",
                     "updatedAt": "2017-12-02T16:52:45.52"
                 },
@@ -75,8 +66,8 @@ class WebSocketEstablishedTests(
                     "memberName": "Ninja",
                     "paidAmount": 500,
                     "forWhat": "beef",
-                    "id": "",
-                    "eventId": "",
+                    "id": "5a226c2d7c245e14f33fc5a8",
+                    "eventId": "5a226c2d7c245e14f33fc5a8",
                     "createdAt": "2017-12-02T16:52:45.52",
                     "updatedAt": "2017-12-02T16:52:45.52"
                 }
@@ -85,114 +76,74 @@ class WebSocketEstablishedTests(
         }
     """, EventRes::class.java)
 
-    fun postEvent() {
-        val event1 = """
+    private val eventReq1 = objectMapper.readValue("""
             {
                 "name": "Koiki Camp"
             }
-        """
+        """, EventReq::class.java)
 
-        val scrooge1 = """
+    private val scroogeReq1 = objectMapper.readValue("""
             {
                 "memberName": "Nabnab",
                 "paidAmount": 200,
                 "forWhat": "rent-a-car"
             }
-        """
+        """, ScroogeReq::class.java)
 
-        val scrooge2 = """
+    private val scroogeReq2 = objectMapper.readValue("""
             {
                 "memberName": "Ninja",
                 "paidAmount": 500,
                 "forWhat": "beef"
             }
-        """
+        """, ScroogeReq::class.java)
 
-        val objectMapper = ObjectMapper()
-        val eventReq: EventReq = objectMapper.readValue(event1, EventReq::class.java)
-        val scroogeReq1: ScroogeReq = objectMapper.readValue(scrooge1, ScroogeReq::class.java)
-        val scroogeReq2: ScroogeReq = objectMapper.readValue(scrooge2, ScroogeReq::class.java)
+    private val prepare: String
+        get() {
+            val location = restTemplate.postForLocation("/events", eventReq1)
 
-        val location = restTemplate.postForLocation("/events", eventReq)
-        log.info("location: $location")
+            val compiledUri = location.toString().split(Pattern.compile("/"))
+            val id = compiledUri[compiledUri.size - 1]
 
-        val aaa = location.toString().split(Pattern.compile("/"))
+            restTemplate.postForLocation("/events/$id/scrooges", scroogeReq1)
+            restTemplate.postForLocation("/events/$id/scrooges", scroogeReq2)
 
-        //val p = Pattern.compile("(\\d+)$")
-        //val group = p.matcher(location.toString()).find()
-        id = aaa[aaa.size-1]
-
-        log.info("resource ID: $id")
-
-        restTemplate.postForLocation("/events/$id/scrooges", scroogeReq1)
-        restTemplate.postForLocation("/events/$id/scrooges", scroogeReq2)
-    }
+            return id
+        }
 
     @Test
     fun test() {
-        this.postEvent()
-        //id = "12345"
+        val scroogeId = this.prepare
 
-        val output: ReplayProcessor<String> = ReplayProcessor.create(5)
-        val input = Flux.range(1, 2).map { index -> "msg-" + index!! }
+        val numberOfRecievedMessages: Long = 1
 
-        val webSocketClient = StandardWebSocketClient()
+        val output: ReplayProcessor<String> = ReplayProcessor.create(numberOfRecievedMessages.toInt())
 
-        val mono: Mono<Void> = webSocketClient.execute(URI(webSocketUri + id), object : WebSocketHandler {
-            override fun handle(session: WebSocketSession) : Mono<Void> {
-                val mono2: Mono<Void> = session
-                        .send(input.map(session::textMessage))
-                        // take(1) ... probably, # of receiving response of websocket
-                        .thenMany(session.receive().take(1).map(WebSocketMessage::getPayloadAsText))
-                        .subscribeWith(output)
-                        .then()
-                return mono2;
-            }
-        })
+        StandardWebSocketClient().execute(URI(webSocketUri + scroogeId)) { session ->
+            session
+                    .toMono()
+                    .thenMany(session.receive().take(numberOfRecievedMessages)
+                            .map(WebSocketMessage::getPayloadAsText))
+                    .subscribeWith(output)
+                    .then()
+        }.block(Duration.ofMillis(5000))
 
-        mono.block(Duration.ofMillis(5000))
+        val websocketResult: EventRes = objectMapper.readValue(
+                output.collectList().block().get(0),
+                EventRes::class.java)
 
-        val result = output.collectList().block(Duration.ofMillis(5000)).get(0)
-
-        log.info("result: $result")
-
-
-
-        val websocketResult: EventRes = objectMapper.readValue(result, EventRes::class.java)
-
-        //assertThat(result.get("name")).isEqualTo("Koiki Camp")
-
-        /*
+        assertThat(websocketResult.createdAt).isBeforeOrEqualTo(LocalDateTime.now())
+        assertThat(websocketResult.updatedAt).isBeforeOrEqualTo(LocalDateTime.now())
+        assertThat(websocketResult.id).isNotNull()
         assertThat(websocketResult)
-                .usingComparatorForFields(object : Comparator<String> {
-                    override fun compare(a: String, b: String): Int {
-                        return 0
-                    }
-                }, "id")
-                .usingComparatorForFields(object : Comparator<LocalDateTime> {
-                    override fun compare(a: LocalDateTime, b: LocalDateTime): Int {
-                        return 0
-                    }
-                }, "createdAt", "updatedAt")
-                .usingComparatorForFields(object : Comparator<List<Scrooge>> {
-                    override fun compare(a: List<Scrooge>, b: List<Scrooge>): Int {
-                        return 0
-                    }
-                }, "scrooges")
-                .isEqualToComparingFieldByField(expectedWsMessage)
-                */
-        assertThat(websocketResult.createdAt)
-                .isBeforeOrEqualTo(LocalDateTime.now())
-        assertThat(websocketResult.updatedAt)
-                .isBeforeOrEqualTo(LocalDateTime.now())
-        assertThat(websocketResult.id)
-                .isNotNull()
-        assertThat(websocketResult)
-                .isEqualToIgnoringGivenFields(expectedWsMessage,
-                        "id", "scrooges", "createdAt", "updatedAt")
+                .isEqualToIgnoringGivenFields(
+                        expectedWsMessage,
+                        "id",
+                        "scrooges", "createdAt", "updatedAt")
 
         assertThat(websocketResult.scrooges)
-                .usingElementComparatorIgnoringFields("id", "eventId", "createdAt", "updatedAt")
+                .usingElementComparatorIgnoringFields("id",
+                        "eventId", "createdAt", "updatedAt")
                 .isEqualTo(expectedWsMessage.scrooges)
 
         websocketResult.scrooges.stream().forEach({
