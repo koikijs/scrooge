407dbf3586a7af6168e779c5bd00c4b424ea4b7f
sideroad <sideroad.jp@gmail.com>
2019-01-14 21:25:45 +0900

    Migrate kyoden front-end on now.sh domain

--- src/main/java/com/koiki/scrooge/event/EventController.java
+++ src/main/java/com/koiki/scrooge/event/EventController.java
@@ -29,7 +29,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping("/events")
 @RequiredArgsConstructor
 @CrossOrigin(
-		origins = "https://kyoden.herokuapp.com",
+		origins = "https://kyoden.now.sh",
 		allowedHeaders = {"Cache-Control", "Content-Language", "Content-Type", "Expires", "Last-Modified", "Pragma", "Location"},
 		exposedHeaders = {"Cache-Control", "Content-Language", "Content-Type", "Expires", "Last-Modified", "Pragma", "Location"},
 		allowCredentials = "true"
